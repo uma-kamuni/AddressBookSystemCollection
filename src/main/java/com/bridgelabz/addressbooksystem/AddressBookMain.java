f730cfa1063ebcfc7495b6df6c8de3792d1b7c4b
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-07 16:14:23 -0800

    Welcome To Address Book Program

---
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbooksystem;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Address Book Program");
+	}
+
+}
